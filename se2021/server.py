be4792df37bd03835957e3573b5494f720f12464
H. Turgut Uyar <uyar@itu.edu.tr>
2021-11-02 15:19:37 +0300

    Add morse encoding endpoint and its test

--- se2021/server.py
+++ se2021/server.py
@@ -2,9 +2,24 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
+MORSE = { "a": ".-",   "b": "-...", "c": "-.-.", "d": "-..",  "e": ".",
+        "f": "..-.", "g": "--.",  "h": "....", "i": "..",   "j": ".---",
+        "k": "-.-",  "l": ".-..", "m": "--",   "n": "-.",   "o": "---",
+        "p": ".--.", "q": "--.-", "r": ".-.",  "s": "...",  "t": "-",
+        "u": "..-",  "v": "...-", "w": ".--",  "x": "-..-", "y": "-.--",
+        "z": "--.." }
+
+def encode_morse(phrase):
+	return " ".join([MORSE[symbol.lower()] for symbol in phrase])
+	
 @app.route('/')
 def hello():
     payload = {'message': 'Hello, world'}
+    return jsonify(payload)
+
+@app.route('/morse/<phrase>')
+def morse(phrase):
+    payload = {"result": encode_morse(phrase)}
     return jsonify(payload)
 
 if __name__ == '__main__':
--- tests/test_api.py
+++ tests/test_api.py
@@ -4,3 +4,8 @@ def test_root_endpoint_should_return_hello_message():
     client = app.test_client()
     response = client.get('/')
     assert response.json == {'message': 'Hello, world'}
+
+def test_morse_endpoint_should_return_three_dots_three_dashes_three_dots_for_sos():
+    client = app.test_client()
+    response = client.get('/morse/sos')
+    assert response.json == {'result': '... --- ...'}
