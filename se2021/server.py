4c678806f111cabc896b08b5b94314d096b3b44e
H. Turgut Uyar <uyar@itu.edu.tr>
2021-11-02 15:49:55 +0300

    Add flake8 and black for style consistency

--- se2021/server.py
+++ se2021/server.py
@@ -2,25 +2,51 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-MORSE = { "a": ".-",   "b": "-...", "c": "-.-.", "d": "-..",  "e": ".",
-        "f": "..-.", "g": "--.",  "h": "....", "i": "..",   "j": ".---",
-        "k": "-.-",  "l": ".-..", "m": "--",   "n": "-.",   "o": "---",
-        "p": ".--.", "q": "--.-", "r": ".-.",  "s": "...",  "t": "-",
-        "u": "..-",  "v": "...-", "w": ".--",  "x": "-..-", "y": "-.--",
-        "z": "--.." }
+MORSE = {
+    "a": ".-",
+    "b": "-...",
+    "c": "-.-.",
+    "d": "-..",
+    "e": ".",
+    "f": "..-.",
+    "g": "--.",
+    "h": "....",
+    "i": "..",
+    "j": ".---",
+    "k": "-.-",
+    "l": ".-..",
+    "m": "--",
+    "n": "-.",
+    "o": "---",
+    "p": ".--.",
+    "q": "--.-",
+    "r": ".-.",
+    "s": "...",
+    "t": "-",
+    "u": "..-",
+    "v": "...-",
+    "w": ".--",
+    "x": "-..-",
+    "y": "-.--",
+    "z": "--..",
+}
+
 
 def encode_morse(phrase):
-	return " ".join([MORSE[symbol.lower()] for symbol in phrase])
-	
-@app.route('/')
+    return " ".join([MORSE[symbol.lower()] for symbol in phrase])
+
+
+@app.route("/")
 def hello():
-    payload = {'message': 'Hello, world'}
+    payload = {"message": "Hello, world"}
     return jsonify(payload)
 
-@app.route('/morse/<phrase>')
+
+@app.route("/morse/<phrase>")
 def morse(phrase):
     payload = {"result": encode_morse(phrase)}
     return jsonify(payload)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run()
--- tests/test_api.py
+++ tests/test_api.py
@@ -1,11 +1,13 @@
 from se2021.server import app
 
+
 def test_root_endpoint_should_return_hello_message():
     client = app.test_client()
     response = client.get('/')
     assert response.json == {'message': 'Hello, world'}
 
-def test_morse_endpoint_should_return_three_dots_three_dashes_three_dots_for_sos():
+
+def test_morse_endpoint_should_return_three_dots_three_dashes_three_dots_for_sos():  # noqa
     client = app.test_client()
     response = client.get('/morse/sos')
     assert response.json == {'result': '... --- ...'}
